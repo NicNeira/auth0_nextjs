b9d5ad173a502f75b4e8301fbac3328072fea9d7
Nicolas Neira <neiranicolas17@gmail.com>
2024-12-06 09:37:11 -0300

    add: env in next.config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    AUTH0_SECRET: process.env.AUTH0_SECRET,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  },
 };
 
 export default nextConfig;
--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,6 +1,8 @@
 // app/api/auth/[...auth0]/route.ts
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
+console.log('AUTH0_SECRET:', process.env.AUTH0_SECRET);
+console.log('AUTH0_CLIENT_ID:', process.env.AUTH0_CLIENT_ID);
 
 export const GET = handleAuth({
   login: handleLogin({
